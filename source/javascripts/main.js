34a9f393620ee88b6fcd05413dc9ce0e5841043a
wkwkrnht <wkwkrnht@github.com>
2023-08-27 01:03:39 +0900

    test

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -118,16 +118,15 @@ function setBoxStyleAtCSS(){ // Set size and position for each air band boxes.
     targets = document.getElementsByClassName('box'), // List of air band boxes
     length = targets.length;
 
-    for( let i = 0; i < length; i++ ){ // Set basic values of air bands style. If display is as landscape, height is fixed, width is valuable, position is set from left.
-        const
-        d1D = targets[i].dataset.down,
-        d1U = targets[i].dataset.up;
+    /*for( let i = 0; i < length; i++ ){ // Set basic values of air bands style. If display is as landscape, height is fixed, width is valuable, position is set from left.
         let
+        d1D = targets[i].dataset.down,
+        d1U = targets[i].dataset.up,
         number = 0; // Value of counting of colision
 
         for( let j = 0; j < length; j++ ){ // Count Colision from sizes of the air band box and others.
             if( i !== j ){
-                const
+                let
                 d2D = targets[j].dataset.down, // DOM proparty of others.
                 d2U = targets[j].dataset.up; // DOM proparty of others.
 
@@ -138,7 +137,7 @@ function setBoxStyleAtCSS(){ // Set size and position for each air band boxes.
         }
 
         targets[i].style.top = calcAmountOfMove(windowHeight, 50, number);
-    }
+    }*/
 }
 
 function main(){ // Main function.
